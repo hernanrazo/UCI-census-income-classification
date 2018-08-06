12d07c5442774a3c3e6b4da534e74c1b923efccb
Hernan Razo <hernanrazo@gmail.com>
2018-08-05 23:32:53 -0500

    Paused graphing to work on categorical data

--- census_pred.py
+++ census_pred.py
@@ -13,19 +13,65 @@ graph_folder_path = '/Users/hernanrazo/pythonProjects/census_income_prediction/g
 print(df.apply(lambda x: sum(x.isnull()), axis = 0))
 print(' ')
 
-print(df.describe())
-print(' ')
+#there are a lot of categorical variables in the
+#dataset. Start off by turning them into numerical values so 
+#plotting the data can be a little easier
 
+#switch income values so they are numerical. '<=50k' is switched to 0 
+#and '>50k' is switched to 1
+
+income_key = {'<=50k':0, '>50k':1}
+
+#for i in df:
+#	i['income'] = i['income'].map(income_key)
+df['income'] = df['income'].map(income_key)
+
+
+print(df.head())
+
+
+
+
+
+
+
+
+
+incomeCountryPie = plt.figure()
+plt.pie(df['income'], labels = df['native.country'])
+plt.savefig(graph_folder_path + 'incomeCountryPie.png')
+
+
+
+
+'''
 #print frequency table of population's education level
 print(df['education.num'].value_counts())
 
-#make a boxplot showing people's income based on age
-edIncomeBoxPlot = plt.figure()
-plt.title('Income Based on Age')
-df.boxplot(column = 'age', by = 'income')
-plt.savefig(graph_folder_path + 'edIncomeBoxPlot')
+#make a histogram showing people's age 
+ageHist = plt.figure()
+plt.title('Age')
+df['age'].hist(bins = 20)
+plt.savefig(graph_folder_path + 'ageHist.png')
+
+#make a bar graph showing income based on gender
+incomeGenderBar = plt.figure()
+incomeGenderBar = pd.crosstab(df['sex'], df['income'])
+incomeGenderBar.plot(kind = 'bar', color = ['red','green'], 
+	grid = False, title = 'Income Based on Gender')
+plt.savefig(graph_folder_path + 'incomeGenderBar.png')
+
+incomeCountryPie = plt.figure()
+plt.pie(df['income'], labels = df['native.country'])
+plt.savefig(graph_folder_path + 'incomeCountryPie.png')
 
 
+incomeEdBar = plt.figure()
+incomeEdBar = pd.crosstab(df['education'], df['income'])
+incomeEdBar.plot(kind = 'bar', color = ['red','green'], 
+	grid = False, title = 'Income Based on Education')
+plt.savefig(graph_folder_path + 'incomeEdBar.png')
+'''
 
 
 
